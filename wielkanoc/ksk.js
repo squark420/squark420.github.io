66cb5e8b69553ad15d828efe058ce2c5270d7d86
Squark420 <f.skwarek.8125@sltzn.katowice.pl>
2023-06-06 13:46:52 +0200

    cum

---
+++ wielkanoc/ksk.js
@@ -0,0 +1,6 @@
+document.getElementById("myButton").addEventListener("click", function () {
+    var dialog = document.getElementById("myDialog");
+    dialog.classList.toggle("active");
+  });
+  
+  
