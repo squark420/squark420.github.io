b983c333870af8258e8bd204fd19ddc26db7e8c4
squark420 <101314905+squark420@users.noreply.github.com>
2025-05-22 11:43:28 +0200

    Update script.js

--- Hania1/script.js
+++ Hania1/script.js
@@ -1,49 +1,5 @@
-const green = document.getElementById("green");
-const red = document.getElementById("red");
-const yellow = document.getElementById("yellow");
-const blue = document.getElementById("blue");
-const triangle = document.getElementById("tri");
+
 const petals = document.getElementById("petals");
-
-green.addEventListener("mouseover", () => {
-    triangle.classList.add("green");
-    triangle.classList.remove("red");
-    triangle.classList.remove("yellow");
-    triangle.classList.remove("blue");
-});
-green.addEventListener("mouseout", () => {
-    triangle.classList.remove("green");
-});
-
-red.addEventListener("mouseover", () => {
-    triangle.classList.remove("green");
-    triangle.classList.add("red");
-    triangle.classList.remove("yellow");
-    triangle.classList.remove("blue");
-});
-red.addEventListener("mouseout", () => {
-    triangle.classList.remove("red");
-});
-
-yellow.addEventListener("mouseover", () => {
-    triangle.classList.remove("green");
-    triangle.classList.remove("red");
-    triangle.classList.add("yellow");
-    triangle.classList.remove("blue");
-});
-yellow.addEventListener("mouseout", () => {
-    triangle.classList.remove("yellow");
-});
-
-blue.addEventListener("mouseover", () => {
-    triangle.classList.remove("green");
-    triangle.classList.remove("red");
-    triangle.classList.remove("yellow");
-    triangle.classList.add("blue");
-});
-blue.addEventListener("mouseout", () => {
-    triangle.classList.remove("blue");
-});
 
 petals.addEventListener("click", () => {
     if (!document.getElementById("petal1").classList.contains("active")) {
